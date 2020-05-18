ac99521e8bf78bb8b60fc49a9c383bc1aa3f1d3a
fariswaliyuddin <60166714+fariswaliyuddin@users.noreply.github.com>
2020-05-18 14:27:29 +0700

    Update editprofile.php

--- profile/editprofile.php
+++ profile/editprofile.php
@@ -6,7 +6,7 @@
  <title>Edit Profile</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-</header>
+</head>
 <body>
     <header>
         <div class="header">
